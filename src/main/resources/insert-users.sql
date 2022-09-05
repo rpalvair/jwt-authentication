c643862116edd0c38141204f7bcf7ad17c9cc075
ruddy palvair <r.palvair@gmail.com>
2022-09-05 18:28:01 +0200

    feat: Change password

--- src/main/resources/insert-users.sql
+++ src/main/resources/insert-users.sql
@@ -1,2 +1,2 @@
 INSERT INTO users(nom,prenom,email,password)
-VALUES('Palvair','Ruddy','r.palvair@gmail.com','$2a$10$rbXGkSRVGKrRMZHjlv8JROUIldHv.euVzatWR.6s282/Gr8nrbnEi');
+VALUES('Palvair','Ruddy','r.palvair@gmail.com','$2a$12$R23P3JhQ8OjvAEs1MxjP0ef0KsrLFfn686X9u//xXzgEDkXC0oNf2');
